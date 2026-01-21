ef18036a6ff9e81e256ecf3dade77e6800953542
Mrskaew <Orawan.duang@gmail.com>
2026-01-21 16:03:26 +0800

    update

--- main.py
+++ main.py
@@ -1,20 +1,2 @@
-#print hello world
-print("hello world")
-
-#print bye!
-print("Bye!")
-
-#new function
-def add_two(num):
-  return num+2
-
-print(add_two(2), add_two(5))
-
-#print result
-print("result")
-
-#new function
-def add_three(num):
-   return num+3
-
-print(add_three(2), add_three(5))
+#greeting
+print("Hi !!!")
